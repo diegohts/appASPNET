7cf7153736b2ac6e7d9a4041015c4173a7597d32
Diego Henrique <diegohrp20@gmail.com>
2021-07-18 00:29:18 -0300

    Initial Commit

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CursoMVC.Models;
+
+namespace CursoMVC.Models
+{
+    public class Context :DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=TRUE");
+        }
+        public DbSet<CursoMVC.Models.Produto> Produto { get; set; }
+    }
+}
